1f05c81f4c72dfbc88dfaa5a3daa4324a83852bb
ic16b055 <ic16b055@technikum-wien.at>
2017-05-01 11:19:55 +0200

    first add

---
+++ mypopen-myclose/mypopen.c
@@ -0,0 +1,64 @@
+/**
+
+* Beispiel 2
+ 
+
+* @author Maria Kanikova <ic16b002@technikum-wien.at>
+* @author Christian Fuhry <ic16b055@technikum-wien.at>
+* @author Sebastian Boehm <ic16b032@technikum-wien.at>
+
+ * @date 2017/05
+ *
+ * @version 0 
+ *
+ * @todo 
+ * 
+ *
+ */
+
+/*
+ * -------------------------------------------------------------- includes --
+ */
+
+#include <stdio.h>
+
+#include "mypopen.h"
+
+/*
+ * --------------------------------------------------------------- defines --
+ */
+
+/*
+ * -------------------------------------------------------------- typedefs --
+ */
+
+/*
+ * --------------------------------------------------------------- globals --
+ */
+
+/*
+ * ------------------------------------------------------------- functions --
+ */
+
+/**
+ *
+ * \Mit Hilfe der Funktionen mypopen() und mypclose() können Sie relativ einfach ein Shell-Kommando ausführen und das Ergebnis direkt in ein Programm einlesen und weiterverarbeiten bzw. Daten, aus einem Programm heraus, an dieses Kommando übergeben. 
+ *
+
+ *
+ */
+mypopen()
+{
+    
+}
+
+mypclose()
+{
+    
+}
+
+
+
+/*
+ * =================================================================== eof ==
+ */
