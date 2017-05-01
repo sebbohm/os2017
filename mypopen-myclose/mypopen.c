a4945c20a5f8cb9161668c5d331f7f32ca466b27
ic16b055 <ic16b055@technikum-wien.at>
2017-05-01 11:45:18 +0200

    Update mypopen.c

--- mypopen-myclose/mypopen.c
+++ mypopen-myclose/mypopen.c
@@ -29,7 +29,11 @@ static FILE * fp = NULL;
  */
 FILE * mypopen(const char * command, const char * type)
 {
-    
+    //mypopen() muß zunächst eine Pipe einrichten (pipe(2)) 
+    //und dann einen Kindprozeß generieren (fork(2)). Im Kindprozeß ist das richtige Ende der Pipe ("r" oder "w") mit stdin bzw. stdout zu assoziieren 
+    //(dup2(2)) und das im 1. Argument spezifizierte Kommando auszuführen (execl(3) oder execv(3)). 
+    //Verwenden Sie - wie die Funktion popen(3) - zum Ausführen des Kommandos die Shell sh(1). 
+    //Als letztes muß mypopen() von einem Filedeskriptor einen passenden FILE * mit fdopen(3) erzeugen. 
  
    return
 }
@@ -37,6 +41,9 @@ FILE * mypopen(const char * command, const char * type)
 int mypclose(FILE *stream)
 {
  
- 
+ //Bei Aufruf von mypclose() soll der aufrufende Prozeß auf die Terminierung des Kindprozesses warten (waitpid(2)). 
+ //Zur Vereinfachung soll immer nur höchstens eine Pipe mit mypopen() geöffnet werden können. 
+ //Stellen Sie dies in ihrer Implementierung sicher. 
+   
     return
 }
